18e3f303c350b221dd6df503168159f088b707ad
Gillian-B <bruyas.gillian@gmail.com>
2020-09-14 17:05:03 +0200

    test5

--- delivery/push_that.sh
+++ delivery/push_that.sh
@@ -3,12 +3,12 @@ echo "Enter your message"
 read message 
 git add --all
 git commit -m "${message}"
-if [ -z "$(git status --porcelain)" ];
-  then
-    echo "CLEAN"
-else
+#if [ -z "$(git status --porcelain)" ];
+#  then
+#    echo "CLEAN"
+#else
   git status
   echo "Pushing data"
   git push 
-fi
+#fi
 
