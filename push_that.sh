87aa4963c3164e136cb535a6886f007c7f8f4d63
Gillian-B <bruyas.gillian@gmail.com>
2020-09-15 14:03:24 +0200

    Test

--- push_that.sh
+++ push_that.sh
@@ -3,12 +3,8 @@ echo "Enter your message"
 read message 
 git add --all
 git commit -m "${message}"
-#if [ -z "$(git status --porcelain)" ];
-#  then
-#    echo "CLEAN"
-#else
-  git status
-  echo "Pushing data"
-  git push origin master
-#fi
+git status
+echo "Pushing data"
+git push origin master
+
 
